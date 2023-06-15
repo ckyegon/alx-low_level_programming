c74206cd6c5d9419a93c3aedcb2385d92c96ac46
ckyegon <collinskyegon26@gmail.com>
2023-06-15 08:27:24 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*main prints what is in put
+*\n -jumps to new line
+*/
+
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle\n");
+
+  return(0);
+}
