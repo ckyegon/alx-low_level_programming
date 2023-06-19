a01ff0b3230a074733fd4e900273f2681438af8c
ckyegon <collinskyegon26@gmail.com>
2023-06-19 01:41:54 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main -Entry point
+* 
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int j;
+
+	j = '0';
+	while (j < '9')
+	{
+		putchar(j);
+		j++;
+		putchar(',');
+
+	}
+	putchar('\n');
+	return (0);
+}
