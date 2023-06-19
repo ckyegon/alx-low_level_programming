d0d971835b1d86137b3674c4acad344ac167bc50
ckyegon <collinskyegon26@gmail.com>
2023-06-19 02:11:39 -0700

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: 0 Always (Success)
+*/
+int main(void)
+{
+	int i;
+	char low;
+
+	for (i = '0'; i < '9'; i++)
+	{
+		putchar(i);
+	}
+	for (low = 'a'; low <= 'f'; low++)
+	{
+		putchar(low);
+	}
+	putchar('\n');
+	return (0);
+}
