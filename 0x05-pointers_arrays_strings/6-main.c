d789288d842adaa47f446098fa34bf1b64bd40da
ckyegon <collinskyegon26@gmail.com>
2023-06-26 23:59:42 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-main.c
+++ 0x05-pointers_arrays_strings/6-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 * main - check the code
 *
@@ -8,7 +9,7 @@ int main(void)
 {
 	char *str;
 
-	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-	print_rev(str);
+	str = "0123456789";
+	puts2(str);
 	return (0);
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,3 +5,4 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void rev_string(char *s);
+void puts2(char *str);
