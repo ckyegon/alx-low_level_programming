80f62a11adeb410cd006bf9f0065dd2a270373b7
ckyegon <collinskyegon26@gmail.com>
2023-07-27 02:42:38 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/_putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * _putchar - checks for character c 
+ * @c - This is the character to be checked
+ * Return - 1 if c is true else -1
+ */
+int _putchar(char c)
+{
+	return (write(1 &c));
+}
