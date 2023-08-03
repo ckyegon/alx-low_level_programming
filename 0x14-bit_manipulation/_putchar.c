c67cc98ce852962ed69234a8a85fb075d8b648f7
ckyegon <collinskyegon26@gmail.com>
2023-08-03 12:30:44 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/_putchar.c
+++ 0x14-bit_manipulation/_putchar.c
@@ -1,10 +1,13 @@
-#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
 /**
- * _putchar - function to check for
- * @c: is character to be checked
- * Return: 1 if true and -1 if an error
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
 int _putchar(char c)
-{	return (write(1, &c, 1));
+{
+	return (write(1, &c, 1));
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,4 +2,5 @@
 #define main_h
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 #endif
